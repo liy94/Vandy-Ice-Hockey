3f0d93688115193a7b3976f297ba9a3fc13829ae
liy94 <yifei.li@vanderbilt.edu>
2023-10-27 00:03:46 -0500

    google sign in removed to see if this deploys

--- vandy-ice-hockey/src/app/page.tsx
+++ vandy-ice-hockey/src/app/page.tsx
@@ -1,27 +1,11 @@
 "use client"; 
 
-import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
-import LoginButton from "./google/login";
-import LogoutButton from "./google/logout";
-import { useEffect } from 'react';
-import { gapi } from 'gapi-script';
 
 const clientId = "884218967366-bdp7ovau82hdbati7a3kfanock9amqak.apps.googleusercontent.com";
 
 export default function Login() {
-
-  useEffect(() => {
-    function start() {
-      gapi.client.init({
-        clientId: clientId,
-        scope: ""
-      })
-    };
-
-    gapi.load('client:auth2', start);
-  });
 
   return (
     <main className={styles.main}>
@@ -41,11 +25,6 @@ export default function Login() {
         </h2>
         <p>Login or register here</p>
       </Link>
-
-      <div className="GoogleLogIn">
-        <LoginButton />
-        <LogoutButton/>
-      </div>
     </main>
   );
 }
