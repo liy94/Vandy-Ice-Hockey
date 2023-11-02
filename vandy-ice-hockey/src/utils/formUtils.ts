9294009b247529fa8053f29210db9d3789e7c883
jesszhang782 <jessica.x.zhang@vanderbilt.edu>
2023-11-02 12:30:08 -0500

    bug fix in registration form

--- vandy-ice-hockey/src/utils/formUtils.ts
+++ vandy-ice-hockey/src/utils/formUtils.ts
@@ -1,15 +1,23 @@
 // functions for making requests to the backend
 
 // server url
-const serverUrl = 'http://localhost:3001';
+const serverUrl = "http://localhost:3001";
 
 // should check whether the user exists in the future
-export async function createUser(name: string, email: string, mobile_number: string, attendance: string, pickup_location: string, has_car: boolean = false, car_capacity: number = 0): Promise<void> {
-  console.log('here')
+export async function createUser(
+  name: string,
+  email: string,
+  mobile_number: string,
+  attendance: string,
+  pickup_location: string,
+  has_car: string,
+  car_capacity: number = 0
+): Promise<void> {
+  console.log("here");
   const response = await fetch(`${serverUrl}/users`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       name,
@@ -18,22 +26,31 @@ export async function createUser(name: string, email: string, mobile_number: str
       attendance,
       has_car,
       car_capacity,
-      pickup_location
-    })
+      pickup_location,
+    }),
   });
 
   if (!response.ok) {
-    throw new Error('Failed to create user');
+    throw new Error("Failed to create user");
   }
 }
 
 // update user with given id
 // right now, hard coded to a valid user id. In the future, will implement authentication and get the id from there
-export async function updateUser(id: string = "652c26e88bab84882a3e8ef1", name: string, email: string, mobile_number: string, attendance:boolean, pickup_location: string, has_car: boolean = false, car_capacity: number = 0): Promise<void> {
+export async function updateUser(
+  id: string = "652c26e88bab84882a3e8ef1",
+  name: string,
+  email: string,
+  mobile_number: string,
+  attendance: boolean,
+  pickup_location: string,
+  has_car: boolean = false,
+  car_capacity: number = 0
+): Promise<void> {
   const response = await fetch(`${serverUrl}/users/${id}`, {
-    method: 'PUT',
+    method: "PUT",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       name,
@@ -42,22 +59,24 @@ export async function updateUser(id: string = "652c26e88bab84882a3e8ef1", name: 
       attendance,
       has_car,
       car_capacity,
-      pickup_location
-    })
+      pickup_location,
+    }),
   });
 
   if (!response.ok) {
-    console.log('Failed to update user');
+    console.log("Failed to update user");
   }
 }
 
 // Returns a user if successful, otherwise returns an empty object
 // right now, id is hardcoded to a valid user id. In the future, will implement authentication and get the id from there
-export async function getUser(id: string = "652c26e88bab84882a3e8ef1"): Promise<object> {
+export async function getUser(
+  id: string = "652c26e88bab84882a3e8ef1"
+): Promise<object> {
   const response = await fetch(`${serverUrl}/users/${id}`);
 
   if (!response.ok) {
-    console.log('Failed to get user');
+    console.log("Failed to get user");
     return {};
   }
 
