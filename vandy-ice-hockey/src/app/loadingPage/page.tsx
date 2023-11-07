c17450e8c46d5465164abe74c5b26e0c4a97ced5
jesszhang782 <jessica.x.zhang@vanderbilt.edu>
2023-11-06 22:22:34 -0600

    loading page

---
+++ vandy-ice-hockey/src/app/loadingPage/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { checkIfUserExists } from "@/utils/loadingPage";
+
+const loadingPage = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+  const email = session?.user?.email ? session?.user.email : "";
+
+  useEffect(() => {
+    // Perform a database check here
+    let dataExists;
+
+    const fetchUser = async () => {
+      dataExists = await checkIfUserExists(email);
+    };
+
+    fetchUser();
+
+    if (dataExists) {
+      router.push("/driverView");
+    } else {
+      router.push("/registrationForm");
+    }
+  }, []);
+
+  return <div>Loading...</div>;
+};
+
+export default loadingPage;
