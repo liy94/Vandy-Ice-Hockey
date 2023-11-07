d19f066b31b94c1442fba081ce4e5a4e2f2d33f6
ubuntu-laptop <danielzhang139@gmail.com>
2023-11-06 18:58:35 -0600

    got all responses and displaying from db

--- vandy-ice-hockey/src/app/responsesView/page.tsx
+++ vandy-ice-hockey/src/app/responsesView/page.tsx
@@ -1,20 +1,24 @@
 "use client"; // This statement appears to be unrelated to the code and can be removed
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link"; // Import Link from Next.js
 import "./response.css";
 import { useRouter } from "next/navigation";
+import { fetchAllUsersWithStatus } from "@/utils/apiUtils";
+import { User } from "../../types/User";
 
 const ResponsesPage: React.FC = () => {
-  const respondents = [
-    { name: "User 1", response: "Option A" },
-    { name: "User 2", response: "Option B" },
-    { name: "User 3", response: "Option C" },
-    // WIll pull from the backend 
-  ];
+  const [respondents, setRespondents] = useState<User[]>([]); // Will pull from the backend
 
   const router = useRouter(); // Access the router for navigation
-
+  useEffect(() => {
+    fetchAllUsersWithStatus().then((response) => { 
+      if (response.status === 200) {
+        setRespondents(response.data)
+      }
+      // console.log(users)
+    });
+  }, []);
   return (
     <div className="responses-page">
       <div className="top-bar">
@@ -26,7 +30,7 @@ const ResponsesPage: React.FC = () => {
       <ul className="response-list">
         {respondents.map((respondent, index) => (
           <li key={index}>
-            <strong>{respondent.name}:</strong> {respondent.response}
+            <strong>{respondent.name}:</strong> {respondent.attendance + ", " + respondent.hasCar + ", " + respondent.location}
           </li>
         ))}
       </ul>
