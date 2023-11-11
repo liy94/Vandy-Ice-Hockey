6572b49d2ae6e0fd1c57ed24bdf22ab54590657e
liy94 <yifei.li@vanderbilt.edu>
2023-11-11 17:27:52 -0600

    now if you are logged in from last time, you also get redirected. Also removed some route.ts code

--- vandy-ice-hockey/src/app/api/auth/[...nextauth]/route.ts
+++ vandy-ice-hockey/src/app/api/auth/[...nextauth]/route.ts
@@ -9,18 +9,6 @@ const handler = NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    async redirect({ url, baseUrl }) {
-      if (url === baseUrl + "/api/auth/signout") {
-        return process.env.NEXT_PUBLIC_BASE_URL || baseUrl;
-      }
-      // For sign-in, redirect to the desired page but prevent looping
-      if (url === baseUrl + "/loadingPage") {
-        return baseUrl + "/loadingPage";
-      }
-      return baseUrl;
-    }
-  }
 });
 
 export { handler as GET, handler as POST };
