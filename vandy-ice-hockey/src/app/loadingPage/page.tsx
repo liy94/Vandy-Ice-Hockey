19bd6947eaf22f9092fc863e5d4ae2ce67e1f7cc
jesszhang782 <jessica.x.zhang@vanderbilt.edu>
2023-11-11 16:36:51 -0600

    fixed loading page logic

--- vandy-ice-hockey/src/app/loadingPage/page.tsx
+++ vandy-ice-hockey/src/app/loadingPage/page.tsx
@@ -8,25 +8,30 @@ import "./loading.css";
 
 const loadingPage = () => {
   const router = useRouter();
-  const { data: session } = useSession();
-  const email = session?.user?.email ? session?.user.email : "";
+  const { data: session, status } = useSession();
 
   useEffect(() => {
-    // Perform a database check here
-    let dataExists;
+    if (status === "authenticated") {
+      const email = session?.user?.email ? session?.user.email : "";
+      let dataExists;
 
-    const fetchUser = async () => {
-      dataExists = await checkIfUserExists(email);
-    };
+      const fetchUser = async () => {
+        dataExists = await checkIfUserExists(email);
 
-    fetchUser();
+        if (dataExists) {
+          router.push("/driverView");
+        } else {
+          router.push("/registrationForm");
+        }
+      };
 
-    if (dataExists) {
-      router.push("/driverView");
-    } else {
-      router.push("/registrationForm");
+      fetchUser();
     }
-  }, []);
+  }, [status, session]);
+
+  if (status === "loading") {
+    return <div className="loading">Loading...</div>;
+  }
 
   return <div className="loading">Loading...</div>;
 };
